d5b3e9ae049e8c47ae2ede0aa2f99aa50bc8c2fb
thientran1010 <54008767+thientran1010@users.noreply.github.com>
2025-06-23 00:24:35 -0400

    Update dqnModel.py

--- lunar_lander/dqnModel.py
+++ lunar_lander/dqnModel.py
@@ -20,21 +20,3 @@ class dqnModel(nn.Module):
         x = F.relu(self.layer2(x))
         return self.layer3(x)
 
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
