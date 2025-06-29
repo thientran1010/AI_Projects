413342829804f27d0ad0894853025da7414c5e78
PatG89 <79527277+PatG89@users.noreply.github.com>
2025-06-29 13:02:09 -0400

    Update unit_tests_model.py

--- lunar_lander/unit_tests_model.py
+++ lunar_lander/unit_tests_model.py
@@ -5,10 +5,12 @@ import dqnModel as model
 class test_model(unittest.TestCase):
 
     def setUp(self):
-        self.test_model =  model.dqnModel(20,30)
+        self.n_observations = 20
+        self.n_actions = 30
+        self.test_model =  model.dqnModel(self.n_observations,self.n_actions)
 
     def test_init_model_trivial_layer1(self):
-        self.assertEqual(20, self.test_model.layer1.in_features)
+        self.assertEqual(self.n_observations, self.test_model.layer1.in_features)
         self.assertEqual(128, self.test_model.layer1.out_features)
 
     def test_init_model_trivial_layer2(self):
@@ -17,7 +19,7 @@ class test_model(unittest.TestCase):
     
     def test_init_model_trivial_layer3(self):
         self.assertEqual(128, self.test_model.layer3.in_features)
-        self.assertEqual(30, self.test_model.layer3.out_features)
+        self.assertEqual(self.n_actions, self.test_model.layer3.out_features)
     
     def test_init_lower_limit_action(self):
         with self.assertRaises(ValueError):
@@ -28,10 +30,10 @@ class test_model(unittest.TestCase):
             test_model = model.dqnModel(-2,10)
 
     def test_forward_output(self):
-        input = torch.rand(1,20)
+        input = torch.rand(1,self.n_observations)
         output = self.test_model.forward(input)
         self.assertIsInstance(output, torch.Tensor)
-        self.assertEqual(output.shape, (1,30))   
+        self.assertEqual(output.shape, (1,self.n_actions))
 
     def test_forward_x_not_tensor(self):
         with self.assertRaises(TypeError):
