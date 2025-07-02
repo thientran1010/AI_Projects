0843c7420e9ec5c2852709b158e9b4b4d6fb2f7f
PatG89 <patrice.gallant89@gmail.com>
2025-07-01 22:49:23 -0400

    adapted tests to proposed changes

---
+++ lunar_lander/unit_tests_agent_new.py
@@ -0,0 +1,95 @@
+import unittest
+import DQNAgent
+import gymnasium as gym
+import dqnModel
+import torch
+import copy
+import hparams
+
+class test_replayMemory(unittest.TestCase):
+    def setUp(self):
+        self.test_replay = DQNAgent.ReplayMemory(5)
+    
+    def test_init_trivial(self):
+        self.assertEqual = (5, self.test_replay.memory.maxlen)
+
+    def test_init_lower_limit(self):
+        with self.assertRaises(ValueError):
+            test_replay = DQNAgent.ReplayMemory(-5)
+
+    def test_push_wrong_tuple_size(self):
+        with self.assertRaises(TypeError):
+            self.test_replay.push(3)
+
+    def test_push_change_memory(self):
+        memory_old_size = len(self.test_replay.memory)
+        state = torch.rand(2,3)
+        next_state = torch.rand(2,3)
+        action = torch.rand(2,3)
+        reward = torch.rand(2,3)
+        self.test_replay.push(state, action, next_state, reward)
+        self.assertGreater(len(self.test_replay.memory), memory_old_size)
+        self.assertEqual(self.test_replay.memory[-1], DQNAgent.Transition(state, action, next_state,reward))
+    
+    def test_sample(self):
+        test_replay = DQNAgent.ReplayMemory(5)
+        state = torch.rand(2,3)
+        next_state = torch.rand(2,3)
+        action = torch.rand(2,3)
+        reward = torch.rand(2,3)
+        for i in range(3):
+            test_replay.push(state, action, next_state, reward)
+        self.assertEqual(len(test_replay.sample(3)), 3)
+
+    def test_sample_higher_limit(self):
+        exceed_memory = len(self.test_replay.memory) + 1
+        with self.assertRaises(ValueError):
+            self.test_replay.sample(exceed_memory)
+
+    def test_sample_lower_limit(self):
+        with self.assertRaises(ValueError):
+            self.test_replay.sample(-2)
+
+
+
+class test_DQNAgent(unittest.TestCase):
+    def setUp(self):
+        self.params =hparams.HParams(3,3,3)
+        self.agent = DQNAgent.DQNAgent(gym.make("LunarLander-v3",render_mode="human"), model_class=r"dqnModel_lunar_lander.pt", hparams = self.params)
+
+    def test_agent_init(self):
+        self.assertIsInstance(self.agent.policy_net, dqnModel.dqnModel)
+        self.assertIsInstance(self.agent.target_net, dqnModel.dqnModel)
+        self.assertEqual(self.agent.n_neurons, self.params.get_n_neurons())
+        self.assertEqual(self.agent.n_layers, self.params.get_n_layers())
+
+    def test_select_action_state_output(self):
+        state =  torch.rand(2,3)
+        output = self.agent.select_action(state)
+        self.assertIsInstance(output, torch.Tensor)
+
+    def test_select_action_state_not_tensor(self):
+        with self.assertRaises(TypeError):
+            self.agent.select_action(5)
+
+    def test_optimize_output_int(self):
+        output = self.agent.optimize_model()
+        self.assertIsInstance(output, int)
+
+    def test_soft_update(self):
+        current_params = copy.deepcopy(self.agent.target_net.state_dict())
+        self.agent.soft_update()
+        new_params = dict(self.agent.target_net.state_dict())
+        for k in self.agent.target_net.state_dict():
+            self.assertFalse(torch.equal(new_params[k], current_params[k]))
+
+    def test_train_param_not_int(self):
+        with self.assertRaises(TypeError):
+            self.agent.train('a')
+
+    def test_plot_loss_curve_invalid_input(self):
+        with self.assertRaises(TypeError):
+            self.agent.plot_loss_curve(['a'])
+
+if __name__ == '__main__':
+    unittest.main()
--- lunar_lander/unit_tests_trainAgent.py
+++ lunar_lander/unit_tests_trainAgent.py
@@ -1,50 +1,37 @@
 import unittest
-#import TrainAgent
-#import HParams
-
-class TrainAgent:
-    def __init__(self, path, num_ep, overwrite, params):
-        self.path = path
-        self.num_episode = num_ep
-        self.overwrite = overwrite
-        self.hparams = params
-    
-class HParams:
-    def __init__(self,n_neurons, n_layers, num_episodes):
-        self.n_neurons = n_neurons
-        self.n_layers = n_layers
-        self.num_episodes = num_episodes
+import train_agent
+import hparams
 
 class test_TrainAgent(unittest.TestCase):
     
     def setUp(self):
-        self.path = 'test path'
+        self.path = 'test_model.pt'
         self.num_ep = 3
         self.overwrite = False
-        self.params = HParams(3,3,3)
-        self.test_ta = TrainAgent(self.path, self.num_ep, self.overwrite, self.params)
+        self.params = hparams.HParams(3,3,3)
+        self.test_ta = train_agent.TrainAgent(self.path, self.num_ep, self.overwrite, self.params)
 
     def test_init_parameters(self):
-        self.assertIsInstance(str, self.test_ta.path)
-        self.assertIsInstance(int, self.test_ta.num_episode)
-        self.assertIsInstance(bool, self.test_ta.overwrite)
-        self.assertIsInstance(HParams, self.test_ta.hparams)
+        self.assertIsInstance(self.test_ta.get_model_path(), str)
+        self.assertIsInstance(self.test_ta.num_episodes, int)
+        self.assertIsInstance(self.test_ta.overwrite, bool)
+        self.assertIsInstance(self.test_ta.hparams, hparams.HParams)
     
     def test_init_path_not_str(self):
         with self.assertRaises(TypeError):
-            test_ta = TrainAgent(5, self.num_ep, self.overwrite, self.params)
+            test_ta = train_agent.TrainAgent(5, self.num_ep, self.overwrite, self.params)
 
     def test_init_n_eps_not_int(self):
         with self.assertRaises(TypeError):
-            test_ta = TrainAgent(self.path, 'a', self.overwrite, self.params)
+            test_ta = train_agent.TrainAgent(self.path, 'a', self.overwrite, self.params)
 
     def test_init_overwrite_not_bool(self):
         with self.assertRaises(TypeError):
-            test_ta = TrainAgent(self.path, self.num_ep, 5, self.params)
+            test_ta = train_agent.TrainAgent(self.path, self.num_ep, 5, self.params)
 
     def test_init_params_not_hparams(self):
         with self.assertRaises(TypeError):
-            test_ta = TrainAgent(self.path, self.num_ep, self.overwrite, 5)
+            test_ta = train_agent.TrainAgent(self.path, self.num_ep, self.overwrite, 5)
 
     def test_train_agent(self):
         pass
@@ -53,3 +40,5 @@ class test_TrainAgent(unittest.TestCase):
         self.assertEqual(self.test_ta.get_model_path(), self.path)
 
     
+if __name__ == '__main__':
+    unittest.main()
